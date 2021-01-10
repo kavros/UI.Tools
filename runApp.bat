6075285aa3c0eb9c9f7cea360709d948abdd6ef7
kavros <akavroulakis@gmail.com>
2021-01-10 12:01:24 +0000

    update script to minimize terminals

--- runApp.bat
+++ runApp.bat
@@ -1,3 +1,3 @@
-start cmd.exe /c http-server -p 4200
-start cmd.exe /c java -jar  rest-service-0.0.1-SNAPSHOT.jar
-start chrome http://localhost:4200/
+start /min cmd.exe /c http-server -p 4200
+start /min cmd.exe /c java -jar  rest-service-0.0.1-SNAPSHOT.jar
+start /min chrome http://localhost:4200/
